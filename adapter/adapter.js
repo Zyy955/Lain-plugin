ee213cca4ad18fd25c30f650a971f4252148df2b
Zyy955 <1072411694@qq.com>
2023-12-29 06:42:22 +0800

    更新 adapter.js

--- adapter/adapter.js
+++ adapter/adapter.js
@@ -5,29 +5,17 @@ import common from '../model/common.js'
 /** 适配器列表 */
 const adapter = []
 
+/** 启动HTTP服务器，加载shamrock、Com微信适配器 */
+adapter.push(async function httpServer () {
+  const WebSocket = (await import('./WebSocket.js')).default
+  return await (new WebSocket()).server()
+})
+
 /** 加载标准输入 */
 adapter.push(async function stdin () {
   const stdin = (await import('./stdin/stdin.js')).default
   await stdin()
   return common.info('标准输入', '加载完成...您可以在控制台输入指令哦~')
-})
-
-/** QQBot适配器 */
-adapter.push(async function QQBot () {
-  try {
-    const cfg = fs.readFileSync(Bot.lain._path + '/QQBot.yaml', 'utf8')
-    Object.entries(Yaml.parse(cfg)).forEach(async ([appid, cfg]) => {
-      if (Object.keys(cfg).length === 0) return
-      const StartQQBot = (await import('./QQBot/index.js')).default
-      return new StartQQBot(cfg)
-    })
-  } catch (err) { return common.error('QQBot', `QQBot适配器加载失败,${err}`) }
-})
-
-/** 启动HTTP服务器，加载shamrock、Com微信适配器 */
-adapter.push(async function httpServer () {
-  const WebSocket = (await import('./WebSocket.js')).default
-  return (new WebSocket()).server()
 })
 
 /** 加载QQ频道适配器 */
@@ -46,30 +34,43 @@ adapter.push(async function QQGuild () {
   common.info('Lain-plugin', 'QQ频道适配器加载完成...')
 })
 
-/** 加载适配器 */
-await Promise.all(adapter.map(i => {
+/** QQBot适配器 */
+adapter.push(async function QQBot () {
   try {
-    return i()
+    const cfg = fs.readFileSync(Bot.lain._path + '/QQBot.yaml', 'utf8')
+    Object.entries(Yaml.parse(cfg)).forEach(async ([appid, cfg]) => {
+      if (Object.keys(cfg).length === 0) return
+      const StartQQBot = (await import('./QQBot/index.js')).default
+      return new StartQQBot(cfg)
+    })
+  } catch (err) { return common.error('QQBot', `QQBot适配器加载失败,${err}`) }
+})
+
+/** 加载微信 */
+adapter.push(async function wechat4u () {
+  const StartWeChat4u = (await import('./WeChat-Web/index.js')).default
+  const _path = fs.readdirSync('./plugins/Lain-plugin/config')
+  const Jsons = _path.filter(file => file.endsWith('.json'))
+  if (Jsons.length > 0) {
+    Jsons.forEach(async i => {
+      const id = i.replace(/\.json$/gi, '')
+      try {
+        await new StartWeChat4u(id, i)
+      } catch (error) {
+        common.error('Lain-plugin', `微信 ${id} 登录失败，已跳过。`)
+        common.error('Lain-plugin', error)
+      }
+    })
+  }
+})
+
+/** 加载适配器 */
+for (let i of adapter) {
+  try {
+    await i()
   } catch (error) {
     common.error('Lain-plugin', error)
-    return Promise.reject(error)
   }
-}))
-
-/** 单独处理加载微信 */
-const StartWeChat4u = (await import('./WeChat-Web/index.js')).default
-const _path = fs.readdirSync('./plugins/Lain-plugin/config')
-const Jsons = _path.filter(file => file.endsWith('.json'))
-if (Jsons.length > 0) {
-  Jsons.forEach(async i => {
-    const id = i.replace(/\.json$/gi, '')
-    try {
-      await new StartWeChat4u(id, i)
-    } catch (error) {
-      common.error('Lain-plugin', `微信 ${id} 登录失败，已跳过。`)
-      common.error('Lain-plugin', error)
-    }
-  })
 }
 
 common.info('Lain-plugin', `Lain-plugin插件${Bot.lain.version}全部初始化完成~`)
